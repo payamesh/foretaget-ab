7653d52a1923ad19e5a04064ddfd88c86d80958f
payamesh <payam.eshtiagh@gmail.com>
2019-03-19 16:30:20 +0100

    update

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-/* $(document).ready(function() {
+ /*$(document).ready(function() {
     $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
         var target = this.hash;
@@ -9,37 +9,39 @@
             window.location.hash = target;
         });
     });
-}); */
+}); 
 
-/* var slides = document.querySelectorAll('#slideVerksamhet .verksamhetBild');
+ var slides = document.querySelectorAll('.siema .occupation__img');
 console.log(slides)
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 2000);
 
 function nextSlide() {
-    slides[currentSlide].className = 'verksamhetBild';
+    slides[currentSlide].className = 'occupation__img';
     currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].className = 'verksamhetBild slideShowing';
-} */
-const mySiema = new Siema({
-    selector: '.siema',
-    duration: 1000,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 1,
-    draggable: false,
-    multipleDrag: false,
-    threshold: 10,
-    loop: true,
-    rtl: false,
-    onInit: () => {},
-    onChange: () => {},
-});
-document.querySelector('.occupation__button-left').addEventListener('click', () => mySiema.prev());
-document.querySelector('.occupation__button-right').addEventListener('click', () => mySiema.next());
-setInterval(() => mySiema.next(), 10500)
+    slides[currentSlide].className = 'occupation__img slideShowing';
+}
 
 $(document).ready(function() {
+    const mySiema = new Siema({
+        selector: '.siema',
+        duration: 1000,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 1,
+        draggable: false,
+        multipleDrag: false,
+        threshold: 10,
+        loop: true,
+        rtl: false,
+        onInit: () => {},
+        onChange: () => {},
+    });
+    document.querySelector('.occupation__button-left').addEventListener('click', () => mySiema.prev());
+    document.querySelector('.occupation__button-right').addEventListener('click', () => mySiema.next());
+    setInterval(() => mySiema.next(), 10500) */
+    $(document).ready(function() {
+
     var time = 2;
     var $bar,
         $slick,
@@ -47,7 +49,7 @@ $(document).ready(function() {
         tick,
         percentTime;
 
-    $slick = $('.slider');
+    $slick = $('.slider-progress');
     $slick.slick({
 
 
@@ -58,7 +60,7 @@ $(document).ready(function() {
 
     $bar = $('.slider-progress .progress');
 
-    $('.slider-wrapper').on({
+    $('.sliderPosition').on({
         mouseenter: function() {
             isPause = true;
         },
@@ -100,7 +102,55 @@ $(document).ready(function() {
 });
 
 
-//imagebuttons
+jQuery(document).ready(function ($) {
+
+    $('#slider-progress').ready(function(){
+      setInterval(function () {
+          moveRight();
+      }, 10500);
+    });
+    
+      var slideCount = $('#sliderPosition ul .occupation__img').length;
+      var slideWidth = $('#sliderPosition ul .occupation__img').width();
+      var slideHeight = $('#sliderPosition ul .occupation__img').height();
+      var sliderUlWidth = slideCount * slideWidth;
+      
+      $('#sliderPosition').css({ width: slideWidth, height: slideHeight });
+      
+      $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+      
+      $('#slider ul img:last-child').prependTo('#slider ul');
+  
+      function moveLeft() {
+          $('#sliderPosition ul').animate({
+              left: + slideWidth
+          }, 200, function () {
+              $('#sliderPosition ul img:last-child').prependTo('#sliderPosition ul');
+              $('#sliderPosition ul').css('left', '');
+          });
+      };
+  
+      function moveRight() {
+          $('#sliderPosition ul').animate({
+              left: - slideWidth
+          }, 200, function () {
+              $('#sliderPosition ul img:first-child').appendTo('#sliderPosition ul');
+              $('#sliderPosition ul').css('left', '');
+          });
+      };
+  
+      $('.occupation__button-left').click(function () {
+          moveLeft();
+      });
+  
+      $('.occupation__button-right').click(function () {
+          moveRight();
+      });
+  
+  });    
+
+
+  //imagebuttons
 let textToChange = document.getElementsByClassName('testimones__p')[0];
 let img3 = document.getElementsByClassName('bullet__button3');
 let img1 = document.getElementsByClassName('bullet__button')[0];
@@ -139,20 +189,3 @@ function changeText3() {
     textToChange.innerHTML = "Vad säger våra kunder? <br> Utmärkt service, rekommenderas starkt. Tack för er hjälp.<br> //Fortfarande fri"
 
 }
-
-(function() {
-
-	var hamburger = {
-		navToggle: document.querySelector('.nav-toggle'),
-		nav: document.querySelector('nav'),
-
-		doToggle: function(e) {
-			e.preventDefault();
-			this.navToggle.classList.toggle('expanded');
-			this.nav.classList.toggle('expanded');
-		}
-	};
-
-	hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
-
-}());
